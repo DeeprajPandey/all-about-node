cb1fce396709b960ec01e6aba65e3d092c420572
Pontiac <15888623+DeeprajPandey@users.noreply.github.com>
2020-08-21 12:21:27 +0530

    Add note on transpilers

--- es6/main.mjs
+++ es6/main.mjs
@@ -15,3 +15,8 @@ sayHi();
 // import sayHi from './util';
 
 // sayHi();
+
+// Note: If you want node to process ES6 without the experimental-modules
+// flag (and for .js instead of .mjs files - you should prefer .mjs if you
+// are creating a module for reasons here: https://v8.dev/features/modules#mjs)
+// then use a transpiler like Babel or switch to TypeScript. :p
